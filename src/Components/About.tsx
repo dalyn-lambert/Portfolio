3c708fb99710cc614acbd581b200a065e7fd50ef
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-10-10 12:58:50 -0500

    created Header and About components

---
+++ src/Components/About.tsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div className='bg-slate-200 text-black p-2 rounded-md flex flex-col'>
+      <span className='font-bold'>About</span>
+      <span>
+        A few sentences about me and how I am developer. I have some cats. 
+        Another sentence or two should do it I think.
+      </span>
+    </div>
+  );
+};
+export default About;
---
+++ src/Components/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div className='bg-slate-200 text-black p-2 rounded-md flex flex-row justify-between'>
+      <div className='flex flex-col'>
+        <span className='text-2xl font-bold'>Dalyn Boyd</span>
+        <span className='text-sm'>Web Developer</span>
+      </div>
+      <div>
+        <span>icon</span>
+        <span>icon</span>
+      </div>
+    </div>
+  );
+};
+export default Header;
