37f6d4575d47ebfaa7e9827c4cf850961cfe6ec7
Dalyn Boyd <dalyn.boyd@gmail.com>
2023-10-10 14:41:18 -0500

    made main bg black

--- src/Components/FeaturedProject.tsx
+++ src/Components/FeaturedProject.tsx
@@ -4,7 +4,7 @@ import InternetIcon from '@/app/Icons/Internet';
 const FeaturedProject = () => {
   return (
     <div className='bg-slate-200 text-black p-2 rounded-md flex flex-col gap-2'>
-      <div className='w-52 h-52 bg-red-100'>Image</div>
+      <div className='w-52 h-52 bg-red-100 '>Image</div>
       <div className='flex flex-row justify-between'>
         <div className='flex flex-col'>
           <span className='text-sm italic'>Featured Project</span>
